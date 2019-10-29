c44bdbe455cfbfde6f82af65f92cd619d8085cfb
toastls93 <toastls93@line.games>
2019-10-29 14:34:16 +0900

    전처리 내용 추가 : rn 2이상 sum(pt) = 0 제외

--- Discovering_Play_Patterns/time_series_cluster.R
+++ Discovering_Play_Patterns/time_series_cluster.R
@@ -1,4 +1,5 @@
 setwd('D:\\git_R\\papers\\Discovering_Play_Patterns')
+options(scipen = 999, digits=21)
 ########################################################################################################
 library(parallelDist) # https://www.rdocumentation.org/packages/parallelDist/versions/0.1.1/topics/parDist
 source("corFuncPtr.R") # 자동으로 패키지 설치 안내문으뜨면 설치하면 된다.
@@ -15,14 +16,11 @@ library(bigrquery) # 군집별 특성을 찾을 때 사용
 ########################################################################################################
 
 
-setwd('D:\\데이터분석\\초기 6시간')
-options(scipen = 999, digits=21)
-
 # 데이터 읽기
  # nid : 유저 아이디
  # rn  : 구분자 번호 (10분단위, 1일단위 등)
  # pt  : rn에 맞는 수치 
-data<- fread('SZ_daily_PT_sum_20191025_2.csv')
+data<- fread('SZ_daily_PT_sum_201909.csv', integer64 = 'numeric')
 colnames(data)<- c('nid', 'rn', 'pt')
 
 # Hyper parameter
@@ -31,6 +29,14 @@ MA <- 3
 
 # 전처리 결과 불러오기 
 data<- df_prepro(data,seq_length = max_rn) # source('df_prepro.R')
+
+
+# 전처리 단계 추가 --> rn 2이상 sum(pt) = 0 이면 data에서 제외
+ # 보통 30 ~ 50% 유저들이 걸러진다.
+nid_pt_sum<-data %>% filter(rn > 1) %>% group_by(nid) %>% summarise(sum_pt = sum(pt))
+nid_pt_sum<- nid_pt_sum %>% filter(sum_pt==0)
+data<- data %>% filter( !(nid %in% nid_pt_sum$nid ))
+
 
 # COR matrix 작성
  # 유저간 trend 데이터를 담아두는 공간
@@ -68,12 +74,12 @@ hc1 <- fastcluster::hclust(d, method = "ward.D" )
 
 # Plot the obtained dendrogram
  # 데이터가 많으면 굳이 안그릴 것을 추천
-plot(hc1, cex = 0.01, hang = -1, main= 'SZ 2019-10 가입유저 - 가입 후 7일 PT', label = FALSE)
-rect.hclust(hc1, k = 3, border = 2:5)
+# plot(hc1, cex = 0.01, hang = -1, main= 'SZ 2019-10 가입유저 - 가입 후 7일 PT', label = FALSE)
+# rect.hclust(hc1, k = 6, border = 2:8)
 
 
 # Cut tree into 3 groups
-sub_grp <- cutree(hc1, k = 3)
+sub_grp <- cutree(hc1, k = 6)
 
 # Number of members in each cluster
 table(sub_grp)
@@ -86,13 +92,24 @@ pt_matrix<- as.matrix(casted_data[, -1])
 
 
 vis_group_1<- group_vis(data, group_num = 1)
-grid.arrange(vis_group_1)
+#grid.arrange(vis_group_1)
 
 vis_group_2<- group_vis(data, group_num = 2)
-grid.arrange(vis_group_2)
+#grid.arrange(vis_group_2)
 
 vis_group_3<- group_vis(data, group_num = 3)
-grid.arrange(vis_group_3)
+#grid.arrange(vis_group_3)
+
+vis_group_4<- group_vis(data, group_num = 4)
+#grid.arrange(vis_group_4)
+
+vis_group_5<- group_vis(data, group_num = 5)
+#grid.arrange(vis_group_5)
+
+vis_group_6<- group_vis(data, group_num = 6)
+#grid.arrange(vis_group_6)
+
+grid.arrange(vis_group_1, vis_group_2, vis_group_3, vis_group_4, vis_group_5, vis_group_6)
 
 
 
