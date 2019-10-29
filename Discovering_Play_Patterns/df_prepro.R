d8bf6af5f8a29b491517609be912efc32d13d079
toastls93 <toastls93@line.games>
2019-10-29 14:32:52 +0900

    JOIN 방식 변경 _> data.table::CJ

--- Discovering_Play_Patterns/df_prepro.R
+++ Discovering_Play_Patterns/df_prepro.R
@@ -5,10 +5,12 @@ df_prepro<- function(df, seq_length = 36){
   colum_name<- colnames(df)
   nid_list<- unique(df$nid)
   
-  nid_and_seq<- merge(nid_list, seq_len(seq_length), all.x = True)
-  nid_and_seq<- nid_and_seq%>%arrange(x, y)
+  #nid_and_seq<- merge(nid_list, seq_len(seq_length), all.x = True)
+  # nid_and_seq<- nid_and_seq%>%arrange(x, y)
+  nid_and_seq<- data.table::CJ(nid = nid_list, rn = seq_len(seq_length))
+  nid_and_seq<- nid_and_seq%>%arrange(nid, rn)
   
-  df<- nid_and_seq%>%left_join(df, by = c('y' = 'rn', 'x' = 'nid') )
+  df<- nid_and_seq%>%left_join(df, by = c('rn' = 'rn', 'nid' = 'nid') )
   
   # 결측치 0으로 대체
   df$pt[is.na(df$pt)]<-0
