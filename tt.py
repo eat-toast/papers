d6134369b47c8c963f676d028dd68c37fc12b13a
Park Isaac <31301664+eat-toast@users.noreply.github.com>
2020-03-22 23:33:11 +0900

    Update tt.py

--- tt.py
+++ tt.py
@@ -11,7 +11,7 @@ pd.set_option('display.max_columns', 100)
 pd.set_option('display.max_colwidth',200)
 
 # 이삭 빅쿼리 접근 키
-json_dir = 'D:\\bigquery'
+json_dir = './bigquery'
 json_path = os.path.join(json_dir, 'EH' + '.json')
 credentials = service_account.Credentials.from_service_account_file(json_path)
 
@@ -32,8 +32,8 @@ with sample as(
     select reg_datekey, datekey, count(distinct nid) as DAU, sum(daily_revenue) as daily_revenue
     from eh_dw.f_user_map
     where login_flag = 1
-    and datekey>= '2019-11-21' and datekey<= '2019-11-28'
-    and reg_datekey <= '2019-11-22'
+    and datekey>= '2019-11-21' and datekey<= '2019-12-31'
+    and reg_datekey = '2019-11-24'
     group by reg_datekey, datekey
   )as A
   INNER JOIN(
@@ -41,8 +41,8 @@ with sample as(
     select reg_datekey, count(distinct nid) as reg_uu
     from eh_dw.f_user_map
     where nru = 1
-    and datekey>= '2019-11-21' and datekey<= '2019-11-28'
-    and reg_datekey <= '2019-11-22'
+    and datekey>= '2019-11-21' and datekey<= '2019-12-31'
+    and reg_datekey = '2019-11-24'
     group by reg_datekey
   )as B
   ON A.reg_datekey = B.reg_datekey
@@ -66,8 +66,8 @@ from (
 INNER JOIN (
   select reg_datekey,datekey, sum(daily_revenue) as daily_revenue
   from eh_dw.f_user_map
-  where datekey>= '2019-11-21' and datekey<= '2019-11-28'
-  and reg_datekey <= '2019-11-22'
+  where datekey>= '2019-11-21' and datekey<= '2019-12-31'
+  and reg_datekey = '2019-11-24'
   group by reg_datekey,datekey
 )as B
 ON A.datekey = B.datekey and A.reg_datekey = B.reg_datekey
@@ -78,18 +78,24 @@ df = df.fillna(0)
 df.head(3)
 df.reg_datekey = df.reg_datekey.dt.strftime('%Y-%m-%d')
 
-# 전처리
-N = df.reg_diff.max() +1 # 데이터 갯수 최댓값
-M = 30 # 예측단위
+# 분석에 사용할 데이터 df_s( df_sample) 로 정의
+df_s = df[df.reg_diff <= 8]
 
-x_bar = df.groupby('reg_datekey')['retention'].mean() # 평균
-sd = df.groupby('reg_datekey')['retention'].var() # 분산
+
+# 전처리
+N = df_s.reg_diff.max() +1 # 데이터 갯수 최댓값
+M = df.shape[0] # 예측단위
+df_N = df_s.groupby('reg_datekey')['reg_diff'].max() # 가입일별 데이터 갯수
+df_N = pd.DataFrame(df_N.values)
+df_N.columns = colnames
+
+
+x_bar = df_s.groupby('reg_datekey')['retention'].mean() # 평균
+sd = df_s.groupby('reg_datekey')['retention'].var() # 분산
 
 alpha_hat = x_bar* ( (x_bar * (1-x_bar)) / sd -1) # alpha 추정값
 beta_hat = (1-x_bar)* ( (x_bar * (1-x_bar)) / sd -1) # beta 추정값
 
-
-# MA(7) ARPU계산
 
 
 # S(T=t) 계산
@@ -97,51 +103,118 @@ beta_hat = (1-x_bar)* ( (x_bar * (1-x_bar)) / sd -1) # beta 추정값
 tt = pd.DataFrame( beta_hat / (alpha_hat + beta_hat))#.reset_index()
 tt = tt.T.reset_index(drop =True)
 
-survival_df = pd.DataFrame(np.repeat(tt.values, M, axis=0))
-survival_df.columns = tt.columns
-survival_df = survival_df.reset_index()
-survival_df = survival_df.rename(columns = {'index':'t'})
-del survival_df.columns.name
-print(survival_df)
+survival_df_s = pd.DataFrame(np.repeat(tt.values, M, axis=0))
+survival_df_s.columns = tt.columns
+survival_df_s = survival_df_s.reset_index()
+survival_df_s = survival_df_s.rename(columns = {'index':'t'})
+del survival_df_s.columns.name
+print(survival_df_s)
 
-colnames = list(survival_df.columns)
+colnames = list(survival_df_s.columns)
 colnames.remove('t')
-
+ncol = len(colnames)
 # ratio_t 계산
-ratio_df = pd.DataFrame(np.repeat(np.zeros((1, 2)), M, axis=0))
-ratio_df.columns = colnames
+ratio_df_s = pd.DataFrame(np.repeat(np.zeros((1, ncol)), M, axis=0))
+ratio_df_s.columns = colnames
 
 for col in colnames :
     for t in range(2, M):
-        ratio_df.loc[t, col] = (beta_hat[col] + t - 1) / (alpha_hat[col] + beta_hat[col] + t -1 )
+        ratio_df_s.loc[t, col] = (beta_hat[col] + t - 1) / (alpha_hat[col] + beta_hat[col] + t -1 )
 
 for col in colnames :
     for i in range(2, M):
-        survival_df.loc[i ,col] = survival_df.iloc[i-1,][col] * ratio_df.iloc[i,][col]
+        survival_df_s.loc[i ,col] = survival_df_s.iloc[i-1,][col] * ratio_df_s.iloc[i,][col]
 
-# survival_df: t > 0
-survival_df = survival_df[survival_df.t > 0]
-
-
+# survival_df_s: if t = 0 : retention is 1.
+survival_df_s.loc[survival_df_s.t == 0, colnames ] = 1
 
 
-
-
-
-
-
-
+#DAU 계산 - df에서 가입일별 DAU 가져오기
+DAU = pd.DataFrame(np.zeros(shape = (df.shape[0], ncol)))
+DAU.columns = colnames
+for col in colnames:
+    idx = df.reg_datekey == col
+    DAU[col] = df.loc[idx, 'DAU']
 
 
 
+#DAU_hat 계산하기
+DAU_hat = pd.DataFrame(np.zeros(shape = (df.shape[0], ncol)))
+DAU_hat.columns = colnames
+
+for col in colnames:
+    idx = df_s.reg_datekey == col
+    DAU_hat[col] = df_s.loc[idx, 'DAU']
+
+    idx2 = np.where(DAU_hat[col].isnull())[0][0] # 첫 True 위치
+
+    for j in range(df.shape[0]):
+        if j > idx2-1 :
+            DAU_hat.loc[j, col] = DAU_hat.loc[0, col] * survival_df_s.loc[j-1, col]
+
+        else:
+            DAU_hat.loc[j, col] = DAU.loc[j, col]
+
+# MA(7) ARPU계산
+ARPU_hat = pd.DataFrame(np.zeros(shape = (M, ncol)))
+ARPU_hat.columns = colnames
+
+# 예측의 결과값 저장
+pred = pd.DataFrame(np.zeros(shape = (M, ncol)))
+pred.columns = colnames
+# pred에 첫 7일은 넣어주기. --> 이미 알고 있는 내용
+
+
+# 실제 데이터 저장
+real = pred.copy(deep = True)
+
+for col in colnames:
+    idx = df_s.reg_datekey == col
+    ARPU_hat[col] = df_s.loc[idx, 'ARPU'].rolling(window = 7, min_periods= 1).mean()
+
+    idx2 = np.where(ARPU_hat[col].isnull())[0][0] # 첫 True 위치
+    max_n = df_N[col].values
+
+    for j in range(df.shape[0]):
+        if j <= max_n :
+            pred.loc[j, col] = ARPU_hat.loc[j, col] * DAU_hat.loc[j, col] * survival_df_s.loc[:j,col].sum()  # 실제값으로 바꾸기!!!
+        elif 6 <= j <= max_n :
+            pred.loc[j, col] = ARPU_hat.loc[j, col] * DAU_hat.loc[j, col] * survival_df_s.loc[:j, col].sum()
+        else:
+            pred.loc[j, col] = ARPU_hat.loc[idx2 - 1, col] * DAU_hat.loc[j, col] * survival_df_s.loc[:j, col].sum()
+
+
+            ### 실제 매출액과 비교
+    real[col] = df.loc[df.reg_datekey == col, 'daily_revenue']
+
+
+
+f, ax = plt.subplots(1, 1)
+plt.plot(real[col])
+plt.plot(pred[col])
+plt.show()
+
+
+### error 계산
 
 
 
 
 
+f, ax = plt.subplots(1, 1)
+plt.plot(survival_df_s[col])
+plt.plot(df['retention'])
+plt.show()
+
+f, ax = plt.subplots(1, 1)
+plt.plot(DAU_hat[col])
+plt.plot(df['DAU'])
+plt.show()
 
 
-# sBG =survival_df['S(T=t)']
+
+
+# sBG =survival_df_s['S(T=t)']
 # sBG = sBG.values
 
 
